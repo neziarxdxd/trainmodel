982de59863143bf06f35872d06637469877073ca
zenraiching <rickdavidsangalang@gmail.com>
2021-07-06 11:16:46 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/trainmodel/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trainmodel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Training Model",
+      home: Scaffold(
+        backgroundColor: Colors.brown,
+      ),
+    );
+  }
+}
