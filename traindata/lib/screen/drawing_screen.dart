c10bc3b09f0b18107221ab97b67c551836c0d7fb
zenraiching <rickdavidsangalang@gmail.com>
2021-07-08 14:17:11 +0800

    drawing update

--- traindata/lib/screen/drawing_screen.dart
+++ traindata/lib/screen/drawing_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:traindata/screen/drawing_paint.dart';
+import 'package:traindata/utils/constant.dart';
 
 class DrawingScreen extends StatefulWidget {
   const DrawingScreen({Key key}) : super(key: key);
@@ -8,8 +10,31 @@ class DrawingScreen extends StatefulWidget {
 }
 
 class _DrawingScreenState extends State<DrawingScreen> {
+  // TODO: add await recognizer
+  final _points = List<Offset>();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("Kulitan Test"),
+        ),
+        body: Container(
+            width: Constants.canvasSize,
+            height: Constants.canvasSize,
+            child: GestureDetector(
+              onPanUpdate: (DragUpdateDetails details) {
+                Offset _localPosition = details.localPosition;
+                setState(() {
+                  _points.add(_localPosition);
+                });
+              },
+              onPanEnd: (DragEndDetails details) {
+                _points.add(null);
+              },
+              child: CustomPaint(
+                painter: DrawingPainter(_points),
+              ),
+            )));
   }
 }
