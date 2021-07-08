1a759d7efa6f9a915beb251b9235f765cee4cbec
zenraiching <rickdavidsangalang@gmail.com>
2021-07-08 10:52:22 +0800

    add

---
+++ traindata/lib/screen/drawing_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DrawingScreen extends StatefulWidget {
+  const DrawingScreen({Key key}) : super(key: key);
+
+  @override
+  _DrawingScreenState createState() => _DrawingScreenState();
+}
+
+class _DrawingScreenState extends State<DrawingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
